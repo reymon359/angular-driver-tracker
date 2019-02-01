e720c0760c5bd081f2682ed693e2956154f3d0ed
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-02-01 16:01:21 +0100

    Coconut - nut = coco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
-
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { log } from 'util';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  lat: number;
+  lng: number;
   title = 'trackerDesk';
+  init = false;
+
+  // taxistas: Observable<any[]>;
+  taxistas: Taxista[] = [];
+  siguiendoA: string = null;
+  siguiendoNombre: string = null;
+  constructor(db: AngularFirestore) {
+    // this.taxistas = db.collection('usuarios').valueChanges();
+    db.collection('usuarios').valueChanges()
+      .subscribe((data: Taxista[]) => {
+        this.taxistas = data;
+        if (!this.init) { // este if solo se ejecuta la primera vez
+          this.lat = data[0].lat;
+          this.lng = data[0].lng;
+          this.init = true;
+        }
+        if (this.siguiendoA) {
+          data.forEach(taxista => {
+            if (taxista.clave === this.siguiendoA) {
+              this.lat = taxista.lat;
+              this.lng = taxista.lng;
+            }
+          });
+        }
+      });
+  }
+  seguir(taxista: Taxista) {
+    this.siguiendoA = taxista.clave;
+    this.siguiendoNombre = taxista.nombre;
+
+    this.lat = taxista.lat;
+    this.lng = taxista.lng;
+
+  }
+  dejarDeSeguir() {
+    this.siguiendoA = null;
+    this.siguiendoNombre = null;
+  }
+}
+
+interface Taxista {
+  nombre: string;
+  clave: string;
+  lat: number;
+  lng: number;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'; // esto no hacia falta
 
+import { AgmCoreModule } from '@agm/core';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyA9KTPtCniv71JJUg1vF58R3m1RgdmzStU'
+    }),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
